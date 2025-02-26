c5f9d3cd5f681311cd2eb913b8e1e02942e14508
Clément <c.vier@students.ephec.be>
2025-02-26 19:56:31 +0100

    Squelette projet angular

---
+++ ng-shot_n_go-app/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  template: `
+    <h1>Welcome to {{title}}!</h1>
+
+    <router-outlet />
+  `,
+  styles: [],
+})
+export class AppComponent implements OnInit {
+  title = 'ng-shot_n_go-app';
+
+  ngOnInit(): void {
+    
+  }
+}
