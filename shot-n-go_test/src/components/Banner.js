d548b00161c8ba36644c90e785fe96ff3e4681a0
Clément <c.vier@students.ephec.be>
2025-03-06 21:52:18 +0100

    avant dernière etape openclassroom

--- shot-n-go_test/src/components/Banner.js
+++ shot-n-go_test/src/components/Banner.js
@@ -1,14 +1,7 @@
-import logo from '../assets/logo.png'
 import '../styles/Banner.css'
 
-function Banner() {
-   const title = "Bienvenue sur Shots'N'Go";
-   return (
-      <div className='sng-banner'>
-         <img src={logo} alt="Shot'N'Go" className="sng-logo" />
-         <h1 className='sng-title'>{title}</h1>
-      </div>
-   );
+function Banner({ children }) {
+	return <div className='lmj-banner'>{children}</div>
 }
 
 export default Banner
