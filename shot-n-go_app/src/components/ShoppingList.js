80df31a95a471120d05f83985f75a922e0e34cd7
BlacF0X <c.mertens@students.ephec.be>
2025-03-24 17:32:30 +0100

    feature #2 remplir et modifier le panier

--- shot-n-go_app/src/components/ShoppingList.js
+++ shot-n-go_app/src/components/ShoppingList.js
@@ -1,8 +1,8 @@
 import { shotList } from '../datas/shotList'
-import PlantItem from './ShotItem'
+import ShotItem from './ShotItem'
 import '../styles/ShoppingList.css'
 
-function ShoppingList() {
+function ShoppingList({cart,updateCart,cart_table,update_table}) {
 	const categories = shotList.reduce(
 		(acc, shot) =>
 			acc.includes(shot.category) ? acc : acc.concat(shot.category),
@@ -17,13 +17,18 @@ function ShoppingList() {
 				))}
 			</ul>
 			<ul className='shot-list'>
-				{shotList.map(({ id, cover, name, alcoholLevel, sweetness }) => (
-					<PlantItem
+				{shotList.map(({ id, cover, name, alcoholLevel, sweetness,price }) => (
+					<ShotItem
 						id={id}
 						cover={cover}
 						name={name}
 						alcoholLevel={alcoholLevel}
 						sweetness={sweetness}
+						price = {price.toFixed(2)}
+						cart = {cart}
+						updateCart= {updateCart}
+						cart_table={cart_table}
+						update_table={update_table}
 					/>
 				))}
 			</ul>
