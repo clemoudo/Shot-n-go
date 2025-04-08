da69646fbdf8ed87cc80bd7f699af813d75b8085
Breit <l.breitenstein@students.ephec.be>
2025-04-08 15:14:30 +0200

    Ajout de PrivateRoute.js et modification de login.jsx

--- shot-n-go_app/src/components/App.js
+++ shot-n-go_app/src/components/App.js
@@ -1,38 +1,44 @@
-	// import Banner from './Banner'
-	// import logo from '../assets/logo.svg'
-	import Footer from './Footer'
-	import Navbar from './Navbar'
-	import Home from './Home'
-	import Menu from './Menu'
-	import Games from './Games'
-	import Queue from './Queue'
-	import TEST_DB from './TEST_DB'
-	import Login from './Login'
-	import { Route, Routes } from "react-router-dom"
-	import Leaderboard from './Leaderboard';
+import Footer from './Footer';
+import Navbar from './Navbar';
+import Home from './Home';
+import Menu from './Menu';
+import Games from './Games';
+import Queue from './Queue';
+import TEST_DB from './TEST_DB';
+import Login from './Login';
+import { Route, Routes } from "react-router-dom";
+import Leaderboard from './Leaderboard';
 
-	function App() {
-		return (
-			<div>
-				{/* <Banner>
-					<img src={logo} alt="Shot'N'Go" className='logo' />
-					<h1 className='title'>Shot'N'Go</h1>
-				</Banner> */}
-				<Navbar />
-				<div className="container">
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/menu" element={ <Menu /> } />
-						<Route path="/games" element={ <Games /> } />
-						<Route path="/queue" element={<Queue />} />
-						<Route path='/TEST_DB' element={<TEST_DB />}/>
-						<Route path='/Login' element={<Login />}/>
-						<Route path="/leaderboard" element={<Leaderboard />} />
-					</Routes>
-				</div>
-				<Footer />
-			</div>
-		)
-	}
+// Importer le AuthProvider
+import { AuthProvider } from './contexts/AuthContext';
+// Importer PrivateRoute
+import PrivateRoute from './components/PrivateRoute'; // Assurez-vous que PrivateRoute.js est dans ce dossier
 
-	export default App
+function App() {
+  return (
+    // Envelopper l'application avec le AuthProvider
+    <AuthProvider>
+      <div>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/games" element={<Games />} />
+            <Route path="/queue" element={<Queue />} />
+            <Route path='/TEST_DB' element={<TEST_DB />} />
+            <Route path='/login' element={<Login />} />
+            
+            {/* Route protégée : Si l'utilisateur est connecté, la route est accessible */}
+            <PrivateRoute path="/leaderboard" element={<Leaderboard />} />
+
+            {/* Si tu as d'autres pages protégées, tu peux les ajouter ici, de la même manière */}
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </AuthProvider>
+  );
+}
+
+export default App;
