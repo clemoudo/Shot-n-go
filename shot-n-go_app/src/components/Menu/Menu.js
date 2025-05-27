234bb9165b8387ecc703617f2740cead63b14fcf
Corentin Mertens <c.mertens@students.ephec.be>
2025-05-27 16:12:53 +0200

    correction des bugs créés par le déplacements des fonctions

--- shot-n-go_app/src/components/Menu/Menu.js
+++ shot-n-go_app/src/components/Menu/Menu.js
@@ -8,21 +8,26 @@ function Menu({ machineState, machineShotsState, cartState, walletState }) {
    const { cart, setCart } = cartState;
    const [selectedMachineId, setSelectedMachineId] = useState("");
 
+   // Wrappers pour injecter setCart
+   const addToCartWrapper = (shotElem, addedAmount) => addToCart(shotElem, addedAmount, setCart);
+   const removeItemWrapper = (shotElem) => removeItem(shotElem, setCart);
+   const deleteItemWrapper = (shotElem) => deleteItem(shotElem, setCart);
+
    return (
       <>
          <Cart 
             selectedMachineId={selectedMachineId} 
             walletState={walletState} 
             cartState={cartState} 
-            addToCart={addToCart} 
-            removeItem={removeItem} 
-            deleteItem={deleteItem} 
+            addToCart={addToCartWrapper} 
+            removeItem={removeItemWrapper} 
+            deleteItem={deleteItemWrapper} 
          />
          <ShoppingList 
             selectedMachineIdState={{ selectedMachineId, setSelectedMachineId }} 
             cartState={cartState} 
-            addToCart={addToCart} 
-            removeItem={removeItem} 
+            addToCart={addToCartWrapper} 
+            removeItem={removeItemWrapper} 
             machineState={machineState} 
             machineShotsState={machineShotsState} 
          />
